6d305d2b52bdd1db81d816d8740097a1655d8534
Alexandre Nunes <alexandrenunes@poli.ufrj.br>
2020-01-13 22:01:39 -0200

    Added math symbols

--- asciimath-tags/MI.js
+++ asciimath-tags/MI.js
@@ -1,5 +1,6 @@
 const trim = require('trim');
 const BaseTag = require('./BaseTag');
+const mathSymbols = require('../mathml-to-asciimath-characters/mathSymbols');
 
 module.exports = class MI extends BaseTag {
   constructor(tag) {
@@ -10,6 +11,17 @@ module.exports = class MI extends BaseTag {
     const { children, value } = this.tag;
     if (children.length > 0) return this.mapChildrenToAsciimath().join('');
 
-    return trim(value);
+    const trimValue = trim(value);
+    return this.getMathSymbol(trimValue) || trimValue;
+  }
+
+  getMathSymbol(trimValue) {
+    return this.getMathSymbolBy('character', trimValue) ||
+      this.getMathSymbolBy('glyph', trimValue);
+  }
+
+  getMathSymbolBy(type, trimValue) {
+    const mathSymbolInfo = mathSymbols.find(op => op[type] === trimValue);
+    return mathSymbolInfo ? mathSymbolInfo['asciimath'] || mathSymbolInfo['glyph'] : null;
   }
 }
