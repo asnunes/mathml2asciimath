7cbdf2c538645e19291ecf2b1434bceae96fdb21
Alexandre Nunes <alexandrenunes@poli.ufrj.br>
2020-02-01 21:00:33 -0300

    Removed node only dependency

--- mathml-tags/index.js
+++ mathml-tags/index.js
@@ -1,14 +1,60 @@
-const fs = require('fs');
-const path = require('path');
+const BaseTag = require('./BaseTag');
+const MAction = require('./MAction');
+const MathTag = require('./Math');
+const MEnclose = require('./MEnclose');
+const MError = require('./MError');
+const MFenced = require('./MFenced');
+const MFrac = require('./MFrac');
+const MGlyph = require('./MGlyph');
+const MI = require('./MI');
+const MMultiscripts = require('./MMultiscripts');
+const MN = require('./MN');
+const MO = require('./MO');
+const MOver = require('./MOver');
+const MPadded = require('./MPadded');
+const MPhantom = require('./MPhantom');
+const MPrescripts = require('./MPrescripts');
+const MRoot = require('./MRoot');
+const MRow = require('./MRow');
+const MSqrt = require('./MSqrt');
+const MSub = require('./MSub');
+const MSubsup = require('./MSubsup');
+const MSup = require('./MSup');
+const MTable = require('./MTable');
+const MTd = require('./MTd');
+const MText = require('./MText');
+const MTr = require('./MTr');
+const MUnder = require('./MUnder');
+const MUnderOver = require('./MUnderOver');
 
-const exposeAllAsciimathTags = () => {
-  return fs.readdirSync(__dirname).reduce((acc, fileName) => {
-    const baseName = path.basename(fileName, '.js').toLowerCase();
-    
-    if (baseName === 'index') return acc;
-    acc[baseName] = require(path.join(__dirname, fileName));
-    return acc;
-  }, {});
+
+module.exports = {
+  'basetag': BaseTag,
+  'maction': MAction,
+  'math': MathTag,
+  'menclose': MEnclose,
+  'merror': MError,
+  'mfenced': MFenced,
+  'mfrac': MFrac,
+  'mglyph': MGlyph,
+  'mi': MI,
+  'mmultiscripts': MMultiscripts,
+  'mn': MN,
+  'mo': MO,
+  'mover': MOver,
+  'mpadded': MPadded,
+  'mphantom': MPhantom,
+  'mprescripts': MPrescripts,
+  'mroot': MRoot,
+  'mrow': MRow,
+  'msqrt': MSqrt,
+  'msub': MSub,
+  'msubsup': MSubsup,
+  'msup': MSup,
+  'mtable': MTable,
+  'mtd': MTd,
+  'mtext': MText,
+  'mtr': MTr,
+  'munder': MUnder,
+  'munderover': MUnderOver,
 };
-
-module.exports = exposeAllAsciimathTags();
